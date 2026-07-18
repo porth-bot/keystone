72c09ccbf4fcef5ab1ae8ac44acb64cc8631fcf9
Joseph Regito David <joseph.reggy23@gmail.com>
2026-07-17 22:34:23 -0500

    Restore Apple-like presentation layer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
 import './refinements.css';
+import './apple-ui.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
